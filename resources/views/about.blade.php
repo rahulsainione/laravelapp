7e57282f70cdd50ad457042b3003ec969f66dfcf
Rahul Saini <rahulsainione1@gmail.com>
2026-07-16 16:26:11 +0530

    this is a second commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,4 @@
+@include('include.header',['from'=>'about'])
 <h1>About Us</h1>
 
 <p>{{$name}}</p>
---
+++ resources/views/include/header.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <!-- Well begun is half done. - Aristotle -->
+     <h3>This is a header from {{$from}}</h3>
+
+</div>
