d1df931bd300c3d6ffdbae760b5110dd657d6deb
Rahul Saini <rahulsainione1@gmail.com>
2026-07-24 11:51:17 +0530

    database user insert action perform

--- app/Http/Controllers/UserTest.php
+++ app/Http/Controllers/UserTest.php
@@ -10,7 +10,7 @@ class UserTest extends Controller
     //
 
     function users(){
-        
+        /*
 //         $users = DB::select("select * from users");
         $query = DB::table('users')->where('phone', '23232');
 
@@ -19,5 +19,22 @@ class UserTest extends Controller
         $users = $query->get();
 
         return view('usertest', ['users'=>$users]);
+        */
+
+
+        $userInsert = DB::table('users')->insert(
+            [
+                'name'=>'test',
+                'email'=>'test@gmail.com',
+                'phone'=>'23232'
+
+            ]
+        );
+
+        if($userInsert){
+            return "User Inserted";
+    } else {
+            return "User Not Inserted";
+        }
     }
-}
+ }
