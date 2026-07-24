0dcbd30a5cc40fdd33acc017830f45af044345ff
Rahul Saini <rahulsainione1@gmail.com>
2026-07-24 12:23:49 +0530

    database insert update delete actions perform

--- app/Http/Controllers/UserTest.php
+++ app/Http/Controllers/UserTest.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
+use App\Models\User;
+
 class UserTest extends Controller
 {
     //
@@ -53,7 +55,7 @@ class UserTest extends Controller
             return "User Not Updated";
         } */
 
-
+/*
 
             $userDelete = DB::table('users')->where('name','test')->delete();
 
@@ -62,5 +64,47 @@ class UserTest extends Controller
             } else {
                 return "User Not Deleted";
             }
+
+            */
+
+            // get data using model
+
+            $users = User::get();
+
+          // insert data using model
+
+        //   $users = User::insert([
+        //         'name'=>'mohit',
+        //         'email'=>'mohit@gmail.com',
+        //         'phone'=>'23232'
+        //     ]);
+
+        //     if($users){
+        //         return "User Inserted";
+        //     } else {
+        //         return "User Not Inserted";
+        //     }
+
+
+        //update data using model
+        //  $userUpdate = User::where('name','mohit')->update([
+        //         'email'=>'mohit.updated@gmail.com'
+        //     ]);
+
+        // if($userUpdate){
+        //     return "User Updated";
+        // } else {
+        //     return "User Not Updated";
+        // }   
+
+        //delete data using model
+        //  $userDelete = User::where('name','mohit')->delete();
+        //  if($userDelete){
+        //      return "User Deleted";
+        //  } else {
+        //      return "User Not Deleted";
+        //  }
+
+         return view('usertest', ['users'=>$users]);
     }
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,8 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+
+public $timestamps = false;
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
